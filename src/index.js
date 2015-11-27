ad41e5107036e13007102090e4d565a7fd87dabc
Anton Medvedev <anton@medv.io>
2015-11-27 13:49:48 +0700

    Add patch func.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import monkberry from 'monkberry';
+import { Delegate } from 'dom-delegate';
+
+function patch(monkberry) {
+  monkberry.constructor.View.prototype.on = function (eventType, selector, handler, useCapture = undefined) {
+    if (!this.delegates) {
+      this.delegates = [];
+      this.nodes.forEach((node, i) => this.delegates[i] = new Delegate(node));
+    }
+
+    this.delegates.forEach((delegate) => delegate.on(eventType, selector, handler, useCapture));
+  };
+
+  monkberry.constructor.View.prototype.off = function (eventType = undefined, selector = undefined, handler = undefined, useCapture = undefined) {
+    this.delegates.forEach((delegate) => delegate.off(eventType, selector, handler, useCapture));
+  };
+}
+
+patch(monkberry);
