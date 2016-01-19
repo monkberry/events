7fe2bcd56618d78f443d02eb38b38ffd7dbb0ae3
Anton Medvedev <anton@medv.io>
2016-01-19 15:03:44 +0700

    Update README.md

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ function patch(monkberry) {
   monkberry.constructor.View.prototype.on = function (eventType, selector, handler, useCapture = undefined) {
     if (!this.delegates) {
       this.delegates = [];
-      this.nodes.forEach((node, i) => this.delegates[i] = new Delegate(node));
+      this.nodes.forEach((node, i) => {
+        if (node.nodeType === 8) {
+          throw 'Can not use event delegating with non-element nodes on first level.';
+        }
+
+        this.delegates[i] = new Delegate(node)});
     }
 
     this.delegates.forEach((delegate) => delegate.on(eventType, selector, handler, useCapture));
