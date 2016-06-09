43f9b8895e5c213864eef33e28b36f00ce3b0adf
Anton Medvedev <anton@medv.io>
2016-06-10 01:24:14 +0700

    Update to support latest monkberry.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import monkberry from 'monkberry';
+import Monkberry from 'monkberry';
 import { Delegate } from 'dom-delegate';
 
-function patch(monkberry) {
-  monkberry.constructor.View.prototype.on = function (eventType, selector, handler, useCapture = undefined) {
-    if (!this.delegates) {
-      this.delegates = [];
-      this.nodes.forEach((node, i) => {
-        if (node.nodeType === 8) {
-          throw 'Can not use event delegating with non-element nodes on first level.';
-        }
+Monkberry.prototype.on = function (eventType, selector, handler, useCapture = undefined) {
+  if (!this.delegates) {
+    this.delegates = [];
+    this.nodes.forEach((node, i) => {
+      if (node.nodeType === 8) {
+        throw 'Can not use event delegating with non-element nodes on first level.';
+      }
 
-        this.delegates[i] = new Delegate(node)});
-    }
+      this.delegates[i] = new Delegate(node)
+    });
+  }
 
-    this.delegates.forEach((delegate) => delegate.on(eventType, selector, handler, useCapture));
-  };
+  this.delegates.forEach((delegate) => delegate.on(eventType, selector, handler, useCapture));
+};
 
-  monkberry.constructor.View.prototype.off = function (eventType = undefined, selector = undefined, handler = undefined, useCapture = undefined) {
-    this.delegates.forEach((delegate) => delegate.off(eventType, selector, handler, useCapture));
-  };
-}
+Monkberry.prototype.off = function (eventType = undefined, selector = undefined, handler = undefined, useCapture = undefined) {
+  this.delegates.forEach((delegate) => delegate.off(eventType, selector, handler, useCapture));
+};
 
-patch(monkberry);
